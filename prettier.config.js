60d2a93fb818f9afeab49fd50fff192abba489a9
sergey_utkin <sergey_utkin@sonm.io>
2018-02-13 19:24:13 +0300

    WAL-58 prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 80,
+    parser: 'typescript',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+};
+
